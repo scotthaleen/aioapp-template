fa7b027f9588d642322c57f036516535e9dd0e3a
Scott Haleen <scott.haleen@gmail.com>
2020-12-04 19:41:53 -0500

    test

--- test-ci.sh
+++ test-ci.sh
@@ -2,15 +2,42 @@
 
 set -ex
 
+
+echo "$(which python)"
+echo "$(which pyenv)"
+echo "$PYENV_ROOT"
+echo "$PATH"
+
+echo "- -- -"
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+echo "$(which python)"
+echo "$(which pyenv)"
+echo "$PYENV_ROOT"
+echo "$PATH"
+
+
 APP=aio_example
 
 ./cookie-test.py "${APP}"
 
-cd "${APP}"
 
+
+cd "${APP}"
 make venv_build
 
 cd "${APP}"
+
+
+echo "$(which python)"
+echo "$(which pyenv)"
+echo "$PYENV_ROOT"
+echo "$PATH"
+
+python --version
+
 tox -e devrun
 
 pyenv uninstall -f "${APP}_project"
